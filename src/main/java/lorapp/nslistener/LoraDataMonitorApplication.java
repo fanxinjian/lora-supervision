ae502dcd3971d47bb3c8a263db3bc7df3b43cac4
Tristan Yu <magix.yu@gmail.com>
2016-12-04 22:46:18 +0800

    Initial Commit

---
+++ src/main/java/lorapp/nslistener/LoraDataMonitorApplication.java
@@ -0,0 +1,12 @@
+package lorapp.nslistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoraDataMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoraDataMonitorApplication.class, args);
+	}
+}
---
+++ src/test/java/lorapp/nslistener/LoraDataMonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package lorapp.nslistener;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoraDataMonitorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
