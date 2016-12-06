39008805f14612f8f496dce0ba4070e2f75e691a
Tristan Yu <magix.yu@gmail.com>
2016-12-06 16:39:32 +0800

    Add Database and repository

--- src/main/java/lorapp/supervision/LoraDataMonitorApplication.java
+++ src/main/java/lorapp/supervision/LoraDataMonitorApplication.java
@@ -1,9 +1,13 @@
-package lorapp.nslistener;
+package lorapp.supervision;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages="lorapp.db.repo")
+@EntityScan(basePackages="lorapp.db.entity")
 public class LoraDataMonitorApplication {
 
 	public static void main(String[] args) {
